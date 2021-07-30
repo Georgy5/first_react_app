7eaacf312b6535667af697b535384b9b2087d0fe
Karl Keller <kgk86@hotmail.com>
2021-07-30 15:54:35 +0200

    Add useEffect() and useState() hooks

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,15 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 
 const App = () => {
+  const [users, setUsers] = useState([])
 
   const fetchUsers = async () => {
     const response = await axios.get('https://reqres.in/api/users?page=1')
+    setUsers(response.data.data);
   }
+
+  useEffect(() => {
+  }, []);
 
   return (
     <div>

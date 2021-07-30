17e520f10cedfb9aec2880c44eed379f6a9a54f6
Karl Keller <kgk86@hotmail.com>
2021-07-30 15:22:04 +0200

    Add axios dependency and import axios to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 
 const App = () => {
+
+  const fetchUsers = async () => {
+    const response = await axios.get('https://reqres.in/api/users?page=1')
+  }
+
   return (
     <div>
       <h1>Hello World</h1>

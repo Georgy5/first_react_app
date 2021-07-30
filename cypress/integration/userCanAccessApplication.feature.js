f001732cba77939d1f7f218998d760b99b4f5563
Karl Keller <kgk86@hotmail.com>
2021-07-30 15:42:55 +0200

    Add test case - expected to display 6 users info

--- cypress/integration/userCanAccessApplication.feature.js
+++ cypress/integration/userCanAccessApplication.feature.js
@@ -7,4 +7,8 @@ describe("User that visits app", () => {
   it('is expected to see "Hello World"', () => {
     cy.get("body").should("contain.text", "Hello World");
   });
+
+  it('is expected to display information about 6 users', () => {
+    cy.get('[data-cy=users-list]').children.should('have.length', 6);
+  });
 });

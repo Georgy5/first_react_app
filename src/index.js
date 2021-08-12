bd9332a076d9c391038cfb8e53aacb56202add08
Karl Keller <kgk86@hotmail.com>
2021-08-12 21:36:58 +0200

    Render <Provider> component Import <Provider> component to make the Redux store available and render both ConnectedComponent and HookedComponent component demos.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import ConnectedComponent from './ConnectedComponent';
+import HookedComponent from './HookedComponent';
+import { Provider } from 'react-redux';
+import configureStore from './state/store/configureStore';
+
+
+const store = configureStore();
+// for manual debugging: window.store = store;
+window.store = store;
 
 const root = ReactDOM.createRoot(
+    <Provider store = {store}>
+        <ConnectedComponent />
+        <HookedComponent />
+    </Provider>,
     document.getElementById('root')
 );
 
